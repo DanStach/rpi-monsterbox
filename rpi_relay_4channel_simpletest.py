f88f6e003f2418c6945b6076fc43c8e6b8fddd91
DanStach <Stachd@gmail.com>
2019-06-07 20:02:10 -0500

    adds 4channel test to relay test

--- rpi_relay_4channel_simpletest.py
+++ rpi_relay_4channel_simpletest.py
@@ -1,45 +1,52 @@
-# Simple test for JBtek 4 Channel DC 5V Relay Module on Raspberry Pi
-import time
 import serial
+import time
 import RPi.GPIO as GPIO
+ChannelPin1 = 4
+ChannelPin2 = 17
+ChannelPin3 = 27
+ChannelPin4 = 22
 
-# Setup GPIO
-GPIO.setmode(GPIO.BMC)
-GPIO.setwarning(False)
-GPIO.setup(6,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(19,GPIO.OUT)
-GPIO.setup(26,GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(ChannelPin1,GPIO.OUT)
+GPIO.setup(ChannelPin2,GPIO.OUT)
+GPIO.setup(ChannelPin3,GPIO.OUT)
+GPIO.setup(ChannelPin4,GPIO.OUT)
 
-
-# test relay1 on/off
-print("relay1 on")
-GPIO.output(6,GPIO.HIGH)
-time.sleep(3)
-print("relay1 off")
-GPIO.output(6,GPIO.LOW)
-time.sleep(3)
-
-# test relay2 on/off
-print("relay2 on")
-GPIO.output(13,GPIO.HIGH)
-time.sleep(3)
-print("relay2 off")
-GPIO.output(13,GPIO.LOW)
+print("init all off")
+GPIO.output(ChannelPin1,GPIO.HIGH)
+GPIO.output(ChannelPin2,GPIO.HIGH)
+GPIO.output(ChannelPin3,GPIO.HIGH)
+GPIO.output(ChannelPin4,GPIO.HIGH)
 time.sleep(3)
 
-# test relay3 on/off
-print("relay3 on")
-GPIO.output(19,GPIO.HIGH)
-time.sleep(3)
-print("relay3 off")
-GPIO.output(19,GPIO.LOW)
-time.sleep(3)
 
-# test relay4 on/off
-print("relay4 on")
-GPIO.output(26,GPIO.HIGH)
+print("LED on")
+GPIO.output(ChannelPin1,GPIO.LOW)
 time.sleep(3)
-print("relay4 off")
-GPIO.output(26,GPIO.LOW)
+print("LED off")
+GPIO.output(ChannelPin1,GPIO.HIGH)
+time.sleep(2)
+
+print("LED on")
+GPIO.output(ChannelPin2,GPIO.LOW)
 time.sleep(3)
+print("LED off")
+GPIO.output(ChannelPin2,GPIO.HIGH)
+time.sleep(2)
+
+print("LED on")
+GPIO.output(ChannelPin3,GPIO.LOW)
+time.sleep(3)
+print("LED off")
+GPIO.output(ChannelPin3,GPIO.HIGH)
+time.sleep(2)
+
+print("LED on")
+GPIO.output(ChannelPin4,GPIO.LOW)
+time.sleep(3)
+print("LED off")
+GPIO.output(ChannelPin4,GPIO.HIGH)
+time.sleep(2)
+
+GPIO.cleanup()
